e06d8ef51afd2cee2042a5f3df93e834d8b07bc2
Ilimsky <ilim.sarykbaev@gmail.com>
2024-10-21 07:01:19 +0600

    Добавлена и проверена сущность Employee. Через Постман работает корректно

--- employee-service/src/main/java/com/example/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/example/employeeservice/controller/EmployeeController.java
@@ -6,19 +6,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.employeeservice.dto.EmployeeDTO;
 import com.example.employeeservice.exception.EmployeeNotFoundException;
 import com.example.employeeservice.service.EmployeeServiceImpl;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class EmployeeController {
     private final EmployeeServiceImpl employeeServiceImpl; 
 
