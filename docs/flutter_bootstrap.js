5c8c6cf3d37e2e8ffc2af1f7cfb765079ff9afac
Ilimsky <ilim.sarykbaev@gmail.com>
2025-01-06 04:03:50 +0600

    06.01.2025 04.03

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2609236028"
+    serviceWorkerVersion: "3793227784"
   }
 });
