523c69e9980b45cd13d7368f744c0ad8e11abdc4
Ilimsky <ilim.sarykbaev@gmail.com>
2025-06-06 21:42:23 +0600

    06.06.2025 21.42

--- revizor_backend/src/main/java/org/example/revizor/model/Role.java
+++
@@ -1,7 +0,0 @@
-package org.example.revizor.model;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_SUPERADMIN
-}
--- revizor_backend/src/main/java/org/example/revizor/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.revizor.repository;
-
-import org.example.revizor.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
