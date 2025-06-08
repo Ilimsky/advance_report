f81a8b30e39446e053e3a03772d0531ebfc4f175
Ilimsky <ilim.sarykbaev@gmail.com>
2025-06-08 14:00:43 +0600

    08.06.2025 14.00

--- inventory_backend/src/main/java/org/example/inventory_backend/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.inventory_backend.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String entityName, Long id) {
-        super(entityName + " with ID " + id + " not found");
-    }
-}
