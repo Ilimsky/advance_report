e2201caa4d3cc029a0e5a49ef99f1144068efae3
Ilimsky <ilim.sarykbaev@gmail.com>
2025-07-13 11:20:08 +0600

    added websocket 13.07.25 11:20

--- inventory_backend/src/main/java/org/example/inventory_backend/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.inventory_backend.repository;
-
-import org.example.inventory_backend.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
