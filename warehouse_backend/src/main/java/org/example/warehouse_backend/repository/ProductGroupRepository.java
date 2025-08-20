83f68b7cb29ab7da159c53a1fa247604492eeb81
Ilimsky <ilim.sarykbaev@gmail.com>
2025-08-20 18:51:25 +0600

    redeploy backend 20.08.2025 18:51

--- warehouse_backend/src/main/java/org/example/warehouse_backend/repository/ProductGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.warehouse_backend.repository;
-
-import org.example.warehouse_backend.entity.ProductGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductGroupRepository extends JpaRepository<ProductGroup, Long> {
-}
