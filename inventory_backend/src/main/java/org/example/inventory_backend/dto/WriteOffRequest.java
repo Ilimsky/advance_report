892c9abfb814e2093ff165f8372e851bc82bed92
Ilimsky <ilim.sarykbaev@gmail.com>
2025-08-25 19:51:41 +0600

    deploy with sked_history 25.08.2025 19:51

--- inventory_backend/src/main/java/org/example/inventory_backend/dto/WriteOffRequest.java
+++
@@ -1,8 +0,0 @@
-package org.example.inventory_backend.dto;
-
-import lombok.Data;
-
-@Data
-public class WriteOffRequest {
-    private String reason;
-}
--- inventory_backend/src/main/java/org/example/inventory_backend/service/DepartmentService.java
+++
@@ -1,7 +0,0 @@
-package org.example.inventory_backend.service;
-
-
-import org.example.inventory_backend.dto.DepartmentDTO;
-
-public interface DepartmentService extends GenericService<DepartmentDTO, Long>{
-}
