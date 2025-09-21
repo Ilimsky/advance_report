7e83949f9f1caa14e4a818b987b9ca8c86e14593
Ilimsky <ilim.sarykbaev@gmail.com>
2025-09-21 20:47:23 +0600

    modified users features 21.09.2025 20:47

--- advance_report_backend/src/main/java/com/example/backend/auth/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.auth;
-
-public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_SUPERADMIN
-}
--- advance_report_backend/src/main/java/com/example/backend/auth/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.auth;
-
-import java.util.List;
-
-public interface UserService {
-    List<UserDto> getAllUsers();
-    UserDto getUserById(Long id);
-}
